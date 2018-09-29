b07e6150d156cbc3c5e43577deca34b646f66299
xiangjian98k <879524580@qq.com>
2018-09-29 15:05:42 +0800

    lx3

--- js/talk.js
+++ js/talk.js
@@ -1,30 +1,19 @@
 window.onload=function () {
     $(".one ol").click(function () {
-        $(".one ol").removeClass("kuang");
-        $(this).addClass("kuang");
-        text=$(this).text();
+        $(this).toggleClass("kuang");
     })
-    let yuefen=$("li.oo").eq(0).children().next().text();
     $("li.oo").click(function () {
         let i=$(this).index();
         $(".dd").css("display","none").eq(i).css("display","block");
         $("li.oo").removeClass("kk");
         $(this).addClass("kk");
-        yuefen=$(this).children().next().text();
-        // console.log(yuefen);
-
     })
-    $(".he").click(function () {
+    $(".he i").click(function () {
         $(".tangchu").slideDown();
     })
     $(".tangchu i").click(function () {
         $(".tangchu").slideUp();
     })
+    console.log($(".tangchu i"));
     // console.log($(".one ol"));
-    // console.log($(".true"));
-    $(".true").click(function () {
-        $(".shij").text(text);
-        $(".yr").text(yuefen);
-        $(".tangchu").slideUp();
-    })
 }
